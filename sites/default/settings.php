441833c044fb472448557b7e4a3b5d619e44e3e9
hardik.choksi <hardik171087@gmail.com>
2017-09-26 15:21:31 +0530

    Module Add

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -290,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '4YFLzGOSSlMUhdFw3qi5useCl6zXoZMzOBtYouzHG5g';
+$drupal_hash_salt = 'vgXvzb6wbdXYEqclATPtrDluP2uWHQqI75TcKTOkSbs';
 
 /**
  * Base URL (optional).
